3a6e54c9398327e939faa06b136f40663bcdac53
Laurian1906 <laur.laury@gmail.com>
2024-01-30 11:23:41 +0200

    Memory Game Cards Checker
    Working on cards checker. If they are not identical is done, now working on the case when the cards have the same image.

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,50 @@
-/* Rotate the block */
-var blockInner = document.getElementsByClassName("block_inner");
-var imgID = document.getElementsByClassName("block_back")[0].children;
+// ----------------- Memory Game Logic -----------------
 
-function rotateBlock(element){
+// Step 1: Declaring the variables
+
+// blockInner is the container of content of the card
+var blockInnerElements = document.getElementsByClassName("block_inner");
+
+// flippedCard represents the element of a card faced up
+var flippedCard = null;
+
+function rotateBlock(element) {
+    // Rotate the card
     element.style.transform = 'rotateY(180deg)';
-    console.log("block is showing the image: ", element);
-}
 
-function rotateBlockInitialPosition(element) {
+    // Grab de id of the card
+    elementID = element.children[1].children[0].id;
+    console.log(elementID);
+    console.log(typeof(elementID));
 
-    // Check if innerBlock is found before accessing its style
-    if (element) {
-        element.style.transform = 'rotateY(0deg)';
-        console.log(element);
-        // setTimeout(2000);
-        // console.log("block is hiding the image");
-    } else {
-        console.error("Inner block not found inside the element:", element);
+
+    // Memorize the flipped card in an object
+    if (!flippedCard){
+        flippedCard = {
+            id: elementID, // id of the card
+            element: element, // the element of the card, we need this for later
+            isCopy: elementID.includes("original") ? false : true, // check if is copy
+            isOriginal: elementID.includes("copy") ? false : true // check if is original
+        }
+
+        console.log("First Card is faced up: ", flippedCard);
     }
+    else{
+        // If the second card is identical with first card
+        if(elementID === flippedCard.id){
+            console.log("Cards are identical!");
+        }
+        // If they are not then turn them facing off.
+        else{
+            console.log("Cards are not identical!");
+
+            setTimeout(function(){
+                element.style.transform = 'rotateY(0deg)';
+                flippedCard.element.style.transform = 'rotateY(0deg)';
+                flippedCard = null; // reset flipped card to null again
+            }, 1000);
+        }
+    }
+
 }
-
-// for ( let i = 0; i < blockInner.length; i++){
-//     blockInner[i].addEventListener("click", function(){
-//         rotateBlock(this);
-
-//         for (let j = 0; j < blockInner.length; j++) {
-//             if (blockInner[j] !== this) {
-//                 rotateBlockInitialPosition(blockInner[j]);
-//             }
-//             else if (blockInner[j] === this) {
-//                 console.log(":)");
-//             }
-//         }
-
-//     });
-// }
 
